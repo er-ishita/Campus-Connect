88cac56c72b52b2842a1736cec72ea5e52b10ad7
er-ishita <ishita.arora1919@gmail.com>
2025-11-11 19:43:05 +0530

    Main.java and Timetable.java added and working well...

---
+++ ProjectMain.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class ProjectMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Timetable timetable = new Timetable();
+
+        System.out.println("Hello user! Quick registration before we start!");
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter your course: ");
+        String course = sc.nextLine();
+        System.out.print("Enter your semester: ");
+        int sem = sc.nextInt();
+
+        System.out.println("\nRegistration Complete! ");
+        System.out.println("| Name: " + name + " \n| Age: " + age + " \n| Course: " + course + " \n| Semester: " + sem);
+
+        while (true) {
+            System.out.println("\nCampus Connect Main Menu");
+            System.out.println("1. Modify Timetable");
+            System.out.println("2. Exit");
+            System.out.print("Enter your choice: ");
+            int ch = sc.nextInt();
+            sc.nextLine();
+
+            switch (ch) {
+                case 1 -> timetable.openTimetableMenu();
+                case 2 -> {
+                    System.out.println("Thank you for using Campus-Connect!");
+                    return;
+                }
+                default -> System.out.println("Invalid choice!Please use appropriate choices only...");
+            }
+        }
+    }
+}
